d78490538efa7cc8118253e94f3861969b35debb
smoylik2 <57191031+smoylik2@users.noreply.github.com>
2020-02-28 18:20:27 +0300

    0.01 master

---
+++ src/headers.js
@@ -0,0 +1,7 @@
+const Headers = () => {
+    return(
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+    );
+}
---
+++ src/image.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Headers = () => {
+    return(
+        <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+    );
+}
+
+export default Headers;
